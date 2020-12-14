23ee04b965e5773d5f9933f00054499d9ec8f469
kubczakn <kubczakn@umich.edu>
2020-12-14 16:26:28 -0500

    fixed errors, haikus could still use work

--- quotes.py
+++ quotes.py
@@ -1,6 +1,7 @@
 import wikiquote
 import random
 import syllables
+from wikiquote import DisambiguationPageException
 
 
 def find_quotes():
@@ -8,12 +9,15 @@ def find_quotes():
     haiku = []
     while i < 3:
         quote_index = wikiquote.random_titles()
-        quotes = wikiquote.quotes(random.choice(quote_index))
+        try:
+            quotes = wikiquote.quotes(random.choice(quote_index))
+        except DisambiguationPageException as e:
+            quotes = wikiquote.qotd()
         while not quotes:
             quotes = wikiquote.quotes(random.choice(quote_index))
         syl = 0
         line = ""
-        for word in quotes[0].split():
+        for word in quotes[0].split(" "):
             if "." in word:
                 syl += syllables.estimate(word)
                 line += word
