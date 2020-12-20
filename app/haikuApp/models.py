33eb69b9fa1bad6bc5bfcffc8be2c0d905e4f307
kubczakn <kubczakn@umich.edu>
2020-12-19 19:36:31 -0500

    first model, fixed secret

--- app/haikuApp/models.py
+++ app/haikuApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+# make models here
+
+class Haiku(models.Model):
+    name = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
