41e0204e8480b9a4b1f1bb0d7d0599286610258e
kubczakn <kubczakn@umich.edu>
2020-12-20 19:40:51 -0500

    draft of categorization, better page formatting

--- app/haikuApp/models.py
+++ app/haikuApp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # make models here
 
 class Haiku(models.Model):
+    type = models.CharField(max_length=50, default='default')
     name = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
 
--- database.py
+++ database.py
@@ -1,6 +1,34 @@
 import os
 import sqlite3
+import wikipedia
+import random
 from sqlite3 import Error
+
+
+# Haiku object
+class Haiku:
+    def __init__(self, kind, content):
+        self.kind = kind
+        self.content = content
+
+
+# Search to determine Haiku type
+def search(name):
+    article = wikipedia.search(name)[0]
+
+    try:
+        summary = wikipedia.summary(article)
+    except wikipedia.DisambiguationError as e:
+        summary = wikipedia.summary(e.options[0])
+    except wikipedia.PageError:
+        return 'Film'
+
+    if summary.find('film') != -1:
+        return 'Film'
+    if summary.find('novel') != -1:
+        return 'Book'
+    else:
+        return 'Person'
 
 
 def create_connection(db_file):
@@ -31,7 +59,8 @@ def read_files(path):
         abs_path = path + '/' + filename
         name = filename.replace('.txt', '')
         file = open(abs_path, 'r')
-        texts[name] = file.read()
+        kind = search(name)
+        texts[name] = Haiku(kind, file.read())
         file.close()
 
     return texts
@@ -40,7 +69,7 @@ def read_files(path):
 def create_haiku(conn, files):
     cur = conn.cursor()
     for filename in files:
-        cur.execute("INSERT INTO HAIKU VALUES (?, ?)", (filename, files[filename]))
+        cur.execute("INSERT INTO HAIKU VALUES (?, ?, ?)", (filename, files[filename].content, files[filename].kind))
         conn.commit()
 
     cur.close()
