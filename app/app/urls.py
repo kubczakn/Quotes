050d1a5179a355cfd75b13872df9bf8e049d339d
kubczakn <kubczakn@umich.edu>
2020-12-22 17:48:02 -0500

    created results page

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from haikuApp import views
 
 urlpatterns = [
+    path('haikuApp/result', views.result),
     path('haikuApp/', views.index),
     path('admin/', admin.site.urls),
 ]
--- app/haikuApp/views.py
+++ app/haikuApp/views.py
@@ -9,7 +9,11 @@ def index(request):
     question_list = Question.objects.filter()
     choice_list = Choice.objects.filter()
     context = {
-        'question_list' : question_list,
-        'choice_list' : choice_list
+        'question_list': question_list,
+        'choice_list': choice_list
     }
     return render(request, 'haikuApp/index.html', context)
+
+
+def result(request):
+    return render(request, 'haikuApp/result.html')
